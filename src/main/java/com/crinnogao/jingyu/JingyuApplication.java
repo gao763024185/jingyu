9813f3dc3ae4be62ee621c95f41f3365254b973e
GaoBaohong <763024185@qq.com>
2024-07-25 20:36:53 +0800

    first commit

---
+++ src/main/java/com/crinnogao/jingyu/JingyuApplication.java
@@ -0,0 +1,13 @@
+package com.crinnogao.jingyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingyuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JingyuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crinnogao/jingyu/JingyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crinnogao.jingyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JingyuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
